89ef4be899a043f63aa580e695891a92993ec4ab
Anshul <anshulusr@gmail.com>
2026-06-11 20:10:21 +0530

    main: basic hello world display on e-ink

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
+use embedded_graphics::{
+	draw_target::DrawTarget,
+	mono_font::{MonoTextStyle, ascii::FONT_10X20},
+	pixelcolor::BinaryColor,
+	prelude::*,
+	primitives::{PrimitiveStyle, Rectangle},
+	text::Text,
+};
+
+struct FramebufferTarget<'a> {
+	data: &'a mut [u8],
+	width: u32,
+	height: u32,
+	bpp: u32,
+}
+
+impl FramebufferTarget<'_> {
+	const WHITE_BYTE: u8 = 0xFF;
+	const BLACK_BYTE: u8 = 0x00;
+}
+
+impl<'a> DrawTarget for FramebufferTarget<'a> {
+	type Color = BinaryColor;
+	type Error = std::convert::Infallible;
+
+	fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+	where
+		I: IntoIterator<Item = Pixel<Self::Color>>,
+	{
+		for Pixel(point, color) in pixels.into_iter() {
+			if point.x >= 0
+				&& point.x < self.width as i32
+				&& point.y >= 0
+				&& point.y < self.height as i32
+			{
+				let index = (point.y as usize * self.width as usize) + point.x as usize;
+
+				if index < self.data.len() {
+					for i in 0..self.bpp {
+						self.data[index * 4 + i as usize] = match color {
+							BinaryColor::On => FramebufferTarget::BLACK_BYTE,
+							BinaryColor::Off => FramebufferTarget::WHITE_BYTE,
+						};
+					}
+				}
+			}
+		}
+		Ok(())
+	}
+}
+
+impl<'a> OriginDimensions for FramebufferTarget<'a> {
+	fn size(&self) -> Size {
+		Size::new(self.width, self.height)
+	}
+}
+
 fn main() {
-	println!("Hello, world!");
+	let fb = linuxfb::Framebuffer::new("/dev/fb0").unwrap();
+
+	let mut data = fb.map().unwrap();
+
+	let mut target = FramebufferTarget {
+		data: &mut data,
+		width: fb.get_size().0,
+		height: fb.get_size().1,
+		bpp: fb.get_bytes_per_pixel(),
+	};
+
+	target.clear(BinaryColor::Off);
+
+	let box_style = PrimitiveStyle::with_stroke(BinaryColor::On, 2);
+	Rectangle::new(Point::new(20, 20), Size::new(360, 260))
+		.into_styled(box_style)
+		.draw(&mut target)
+		.unwrap();
+
+	let text_style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
+	Text::new("Hello World!", Point::new(40, 50), text_style)
+		.draw(&mut target)
+		.unwrap();
 }
